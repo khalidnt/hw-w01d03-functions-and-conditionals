b40078a888265c68cfe8a9714ea82c74fa6c0252
khalidnt <405795+khalidnt@users.noreply.github.com>
2018-11-14 11:31:02 +0300

    h3

---
+++ main.js
@@ -0,0 +1,18 @@
+var num1 
+var num2 
+
+function maxOfTwoNumbers(num1, num2) {
+if (num1 > num2 )
+console.log( num1);
+}
+
+else if {
+    ( num1 == num2)
+console.log('numbers are equal')}
+else  {
+
+ 
+    console.log( num2);}
+
+
+    function languagecode()
